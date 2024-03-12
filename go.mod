2803a7c63b6e6b3d45cf4a3de73dcec240886763
Nicole <nicolasaelf64@gmail.com>
2024-03-12 08:49:01 -0600

    small kitten pkg that uses cat

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nfrazier08/kitten
 
 go 1.20
+
+require github.com/nfrazier08/cat v0.0.0-20240312144238-04b4ce330b46 // indirect
--- kitten.go
+++ kitten.go
@@ -1,9 +1,21 @@
 package kitten
 
+import (
+	"github.com/nfrazier08/cat"
+)
+
 func Meow() string {
 	return "Meow!"
 }
 
 func Meows() string {
 	return "Meow! Meow! Meow!"
+}
+
+func BigMeow() string {
+	return cat.WhenGrownUp(Meow())
+}
+
+func BigMeows() string {
+	return cat.WhenGrownUp(Meows())
 }
